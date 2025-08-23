7a7555cc969403c54be092554ead19a9dccf8539
ShirinAjou <ajoushirin@gmail.com>
2025-08-23 21:14:36 +0200

    update

--- src/Card/Game.php
+++ src/Card/Game.php
@@ -6,6 +6,9 @@ use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use App\Card\DeckOfCards;
 use App\Card\Turn;
 
+/**
+ * Class handling game logic. 
+ */
 class Game
 {
     private $session;
@@ -15,6 +18,15 @@ class Game
         $this->session = $session;
     }
 
+    /**
+     * Plays a round of the game.
+     *
+     * @param DeckOfCards $deck The deck of cards used in the game.
+     * @param Turn $turn The current turn in the game.
+     * @param bool $drawCard Whether the player draws a card.
+     * @param bool $stop Whether the player stops their turn.
+     * @return bool True if the game round is completed.
+     */
     public function playGame(DeckOfCards $deck, Turn $turn, bool $drawCard, bool $stop): bool
     {
         if ($drawCard) {
@@ -33,6 +45,13 @@ class Game
         return true;
     }
 
+    /**
+     * Determines the winner of the game based on scores.
+     *
+     * @param int $playerScore The player's score.
+     * @param int $bankScore The bank's score.
+     * @return string The winner of the game with a string.
+     */
     public function winner(int $playerScore, int $bankScore): string
     {
         if ($playerScore > 21) {
